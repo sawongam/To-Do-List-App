e16e7eee174dac1f50f3288791ef827e844df0e7
Sangam Adhikari <sangamadhikari.61@gmail.com>
2024-02-22 17:37:21 +0545

    app finalized

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
 import 'package:sqflite/sqflite.dart';
 
 class HomePage extends StatefulWidget {
@@ -11,7 +12,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   late Database db;
-  List<Map> tasks = [];
+  late List<Map> tasks = [];
 
   final TextEditingController _taskController = TextEditingController();
 
@@ -26,75 +27,74 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: InkWell(
-            onTap: () {
-              deleteAllData();
-            },
-            child: const Text('To-Do App')),
+        title: const Text('To-Do App'),
         backgroundColor: Colors.blueGrey[900],
         titleTextStyle: const TextStyle(
           color: Colors.white,
-          fontSize: 28,
+          fontSize: 25,
           fontWeight: FontWeight.bold,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showDialog(
-              context: context,
-              builder: (context) {
-                return AlertDialog(
-                  title: const Text(
-                    'Add Task',
-                    textAlign: TextAlign.center,
-                  ),
-                  content: TextField(
-                    controller: _taskController,
-                    decoration: const InputDecoration(
-                      hintText: 'Enter your task',
-                      hintStyle: TextStyle(
-                          color: Colors.white,
-                          fontSize: 15,
-                          fontWeight: FontWeight.normal),
-                    ),
-                    style: const TextStyle(
-                      color: Colors.white,
-                    ),
-                  ),
-                  backgroundColor: Colors.blueGrey[900],
-                  titleTextStyle: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),
-                  actions: [
-                    Center(
-                      child: ElevatedButton(
-                        onPressed: () {
-                          insertData(_taskController.text);
-                          Navigator.pop(context);
-                        },
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.white24,
-                          foregroundColor: Colors.white,
-                          elevation: 3,
+      floatingActionButton: tasks.isNotEmpty
+          ? FloatingActionButton(
+              onPressed: () {
+                showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        title: const Text(
+                          'Add Task',
+                          textAlign: TextAlign.center,
                         ),
-                        child: const Text('Add'),
-                      ),
-                    ),
-                  ],
-                );
-              });
-        },
-        child: const Icon(
-          Icons.add,
-          color: Colors.white70,
-        ),
-        backgroundColor: Colors.blueGrey[900],
-        elevation: 7,
-      ),
+                        content: TextField(
+                          controller: _taskController,
+                          decoration: const InputDecoration(
+                            hintText: 'Enter your task',
+                            hintStyle: TextStyle(
+                                color: Colors.white,
+                                fontSize: 15,
+                                fontWeight: FontWeight.normal),
+                          ),
+                          style: const TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                        backgroundColor: Colors.blueGrey[900],
+                        titleTextStyle: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        actions: [
+                          Center(
+                            child: ElevatedButton(
+                              onPressed: () {
+                                Navigator.pop(context);
+                                insertData(_taskController.text);
+                                _taskController.clear();
+                              },
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.white24,
+                                foregroundColor: Colors.white,
+                                elevation: 3,
+                              ),
+                              child: const Text('Add'),
+                            ),
+                          ),
+                        ],
+                      );
+                    });
+              },
+              backgroundColor: Colors.blueGrey[800],
+              elevation: 7,
+              child: const Icon(
+                Icons.add,
+                color: Colors.white70,
+              ),
+            )
+          : null,
       body: Padding(
-        padding: EdgeInsets.all(20.0),
+        padding: const EdgeInsets.all(20.0),
         child: Container(
           decoration: BoxDecoration(
             color: Colors.blueGrey[900],
@@ -104,75 +104,166 @@ class _HomePageState extends State<HomePage> {
                 color: Colors.grey[900]!,
                 blurRadius: 25,
                 spreadRadius: 1,
-                offset: Offset(3, 2),
+                offset: const Offset(3, 2),
               ),
             ],
           ),
-          child: tasks.isNotEmpty
-              ? ListView.builder(
-                  shrinkWrap: true,
-                  itemCount: tasks.length,
-                  itemBuilder: (context, index) {
-                    int id = tasks[index]['id'];
-                    String task = tasks[index]['title'];
-                    int isDone = tasks[index]['isDone'];
-                    bool isDoneBool = isDone == 1;
-                    return GestureDetector(
-                      onLongPress: () {
-                        deleteData(id);
-                      },
-                      child: Card(
-                        color: Colors.blueGrey[900],
-                        child: ListTile(
-                          leading: InkWell(
-                            onTap: () {},
-                            child: Container(
-                              height: 35,
-                              width: 5,
-                              color: Colors.green,
-                            ),
-                          ),
-                          title: Text(
-                            task,
-                            style: !isDoneBool
-                                ? const TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.bold,
-                                  )
-                                : const TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.bold,
-                                    decoration: TextDecoration.lineThrough,
-                                    decorationStyle: TextDecorationStyle.solid,
-                                    decorationColor: Colors.white,
-                                    decorationThickness: 2,
-                                  ),
-                          ),
-                          trailing: CupertinoSwitch(
-                            value: isDoneBool,
-                            onChanged: (value) {
-                              setState(() {
-                                isDoneBool = !isDoneBool;
-                              });
-                              updateIsDone(id, isDone);
+          child: FutureBuilder(
+            future: readInitialData(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return tasks.isNotEmpty
+                    ? ListView.builder(
+                        shrinkWrap: true,
+                        itemCount: tasks.length,
+                        itemBuilder: (context, index) {
+                          int id = tasks[index]['id'];
+                          String task = tasks[index]['title'];
+                          int isDone = tasks[index]['isDone'];
+                          bool isDoneBool = isDone == 1;
+                          return GestureDetector(
+                            onLongPress: () {
+                              deleteData(id);
                             },
-                            activeColor: Colors.green,
-                          ),
+                            child: Card(
+                              color: Colors.blueGrey[900],
+                              child: ListTile(
+                                leading: InkWell(
+                                  onTap: () {},
+                                  child: Container(
+                                    height: 35,
+                                    width: 5,
+                                    color: isDoneBool
+                                        ? Colors.green
+                                        : Colors.orange,
+                                  ),
+                                ),
+                                title: Text(
+                                  task,
+                                  style: !isDoneBool
+                                      ? const TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 15,
+                                          fontWeight: FontWeight.bold,
+                                        )
+                                      : const TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 15,
+                                          fontWeight: FontWeight.bold,
+                                          decoration:
+                                              TextDecoration.lineThrough,
+                                          decorationStyle:
+                                              TextDecorationStyle.solid,
+                                          decorationColor: Colors.white,
+                                          decorationThickness: 2,
+                                        ),
+                                ),
+                                trailing: CupertinoSwitch(
+                                  value: isDoneBool,
+                                  onChanged: (value) {
+                                    setState(() {
+                                      isDoneBool = !isDoneBool;
+                                    });
+                                    updateIsDone(id, isDone);
+                                  },
+                                  activeColor: Colors.green,
+                                ),
+                              ),
+                            ),
+                          );
+                        })
+                    : Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Text(
+                              'No task added yet!',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 15,
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            ElevatedButton(
+                              onPressed: () {
+                                showDialog(
+                                    context: context,
+                                    builder: (context) {
+                                      return AlertDialog(
+                                        title: const Text(
+                                          'Add Task',
+                                          textAlign: TextAlign.center,
+                                        ),
+                                        content: TextField(
+                                          controller: _taskController,
+                                          decoration: const InputDecoration(
+                                            hintText: 'Enter your task',
+                                            hintStyle: TextStyle(
+                                                color: Colors.white,
+                                                fontSize: 15,
+                                                fontWeight: FontWeight.normal),
+                                          ),
+                                          style: const TextStyle(
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                        backgroundColor: Colors.blueGrey[900],
+                                        titleTextStyle: const TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                        actions: [
+                                          Center(
+                                            child: ElevatedButton(
+                                              onPressed: () {
+                                                Navigator.pop(context);
+                                                insertData(
+                                                    _taskController.text);
+                                                _taskController.clear();
+                                              },
+                                              style: ElevatedButton.styleFrom(
+                                                backgroundColor: Colors.white24,
+                                                foregroundColor: Colors.white,
+                                                elevation: 3,
+                                              ),
+                                              child: const Text('Add'),
+                                            ),
+                                          ),
+                                        ],
+                                      );
+                                    });
+                              },
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.blueGrey[800],
+                                foregroundColor: Colors.white,
+                                elevation: 3,
+                              ),
+                              child: const Text('Add Task'),
+                            )
+                          ],
                         ),
-                      ),
-                    );
-                  })
-              : const Center(
-                  child: Text(
-                    'No tasks added yet!',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 15,
-                    ),
-                  ),
-                ),
+                      );
+              } else {
+                return Shimmer.fromColors(
+                  baseColor: Colors.grey[800]!,
+                  highlightColor: Colors.grey[700]!,
+                  child: ListView.builder(
+                      itemCount: 5,
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          title: Container(
+                            height: 45,
+                            color: Colors.grey[900],
+                          ),
+                        );
+                      }),
+                );
+              }
+            },
+          ),
         ),
       ),
     );
@@ -186,7 +277,15 @@ class _HomePageState extends State<HomePage> {
       await db.execute(
           'CREATE TABLE tasks (id INTEGER PRIMARY KEY, title TEXT, isDone INTEGER)');
     });
+    readInitialData();
     readData();
+  }
+
+  Future<List<Map>> readInitialData() async {
+    List<Map> tempTasks =
+        List<Map>.from(await db.rawQuery('SELECT * FROM tasks'));
+    tempTasks.sort((a, b) => a['isDone'].compareTo(b['isDone']));
+    return tempTasks;
   }
 
   void readData() async {
--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         scaffoldBackgroundColor: Colors.blueGrey[900],
